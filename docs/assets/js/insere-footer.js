abe1133072545b9e673eb726d20ef83e9713fda1
Sergio Herman <sergioherman@id.uff.br>
2023-05-24 03:25:21 -0300

    Inserção de conteúdo de EDO - massa-mola-amortecedor. Tabulação de parágrafo em todas as páginas. Criação de js para footer e títulos em diretório nivel 3, assim como o modelo genérico de nivel 3. Formatação de imagem na main.css.

--- docs/assets/js/insere-footer.js
+++ docs/assets/js/insere-footer.js
@@ -59,12 +59,44 @@ const conteudoFooterNv2 = `
         </footer>
 `;
 
+const conteudoFooterNv3 = `
+        <footer>
+            <div id="ft-col1">
+                <p><a href="historia-do-projeto.html">História do Projeto</a></p>
+                <p id="email"><a href="mailto:redmatuff@gmail.com">redmatuff@gmail.com</a></p>
+                <p><a href="direitos-autorais.html">Direitos Autorais</a></p>
+            </div>
+            <div id="ft-col2">
+                <p>Descubra a matemática em uma nova dimensão explorando recursos interativos e ferramentas digitais para aprimorar seu aprendizado. Deixe a tecnologia elevar sua compreensão e ampliar sua visão navegando por um mundo de possibilidades para conquistar o domínio dessa ciência fascinante.</p>
+            </div>
+            <div id="ft-col3">
+                <div class="logos-redes-soc">
+                    <a href="https://www.instagram.com/monitoriaredmatuff/" target="_blank">
+                        <img src="../../../assets/img/footer/InstaIncon.png" alt="Logo Instagram">
+                    </a>
+
+                    <a href="https://www.youtube.com/playlist?list=PLgmoVZEvjejdNccFF8bdiCOqTgGn5SWOO" target="_blank">
+                        <img src="../../../assets/img/footer/ytIncon.png" alt="Logo YouTube">
+                    </a>
+                    
+                    <a href="https://www.geogebra.org/m/tuyvrnbp" target="_blank">
+                        <img src="../../../assets/img/footer/geogebraIncon2.png" alt="Logo Geogebra">
+                    </a>
+                </div>
+                <div><a href="https://www.uff.br/" target="_blank"><img class="logo-uff" src="../../../assets/img/footer/uffIncon.png" alt="Logo da UFF"></a></div>
+            </div>
+        </footer>
+`;
+
+
 try {
     document.querySelector('.footer').innerHTML = conteudoFooter;
-} catch{
-}
+} catch{};
 
 try {
     document.querySelector('.footer-nv2').innerHTML = conteudoFooterNv2;
-} catch{
-}
+} catch{};
+
+try {
+    document.querySelector('.footer-nv3').innerHTML = conteudoFooterNv3;
+} catch{};
--- docs/assets/js/insere-titulos.js
+++ docs/assets/js/insere-titulos.js
@@ -7,12 +7,20 @@ const conteudoTitulos = `
 	</div>
 `;
 
+const conteudoTitulosNv3 = `
+	<div class = "container-titulos">
+		<div class="titulos">
+			<h1><a href="../../../index.html">Cálculo Diferencial</a></h1>
+			<h2>RECURSOS EDUCACIONAIS DIGITAIS PARA O ENSINO DE MATEMÁTICA</h2>
+		</div>
+	</div>
+`;
+
+
 try {
 document.querySelector('.container-titulos').innerHTML = conteudoTitulos;
-} catch{
-    
-}
+} catch{};
 
-
-
-
+try {
+	document.querySelector('.container-titulos-nv3').innerHTML = conteudoTitulosNv3;
+} catch {};
