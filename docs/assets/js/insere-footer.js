f6e540bec8889fd62d1896a02a1af9a827d9aaef
Sergio Herman <sergioherman@id.uff.br>
2023-06-04 22:28:57 -0300

    Criação da página modelo para subditerório de nível 4. Adaptação da primeira página do antigo conteúdo para a nova estilização.

--- docs/assets/js/insere-footer.js
+++ docs/assets/js/insere-footer.js
@@ -88,6 +88,35 @@ const conteudoFooterNv3 = `
         </footer>
 `;
 
+const conteudoFooterNv4 = `
+        <footer>
+            <div id="ft-col1">
+                <p><a href="../../../outras/historia-do-projeto.html">História do Projeto</a></p>
+                <p id="email"><a href="mailto:redmatuff@gmail.com">redmatuff@gmail.com</a></p>
+                <p><a href="../../../outras/direitos-autorais.html">Termos de Licensa de Uso</a></p>
+            </div>
+            <div id="ft-col2">
+                <p>Descubra a matemática em uma nova dimensão explorando recursos interativos e ferramentas digitais para aprimorar seu aprendizado. Deixe a tecnologia elevar sua compreensão e ampliar sua visão navegando por um mundo de possibilidades para conquistar o domínio dessa ciência fascinante.</p>
+            </div>
+            <div id="ft-col3">
+                <div class="logos-redes-soc">
+                    <a href="https://www.instagram.com/monitoriaredmatuff/" target="_blank">
+                        <img src="../../../../assets/img/footer/InstaIncon.png" alt="Logo Instagram">
+                    </a>
+
+                    <a href="https://www.youtube.com/playlist?list=PLgmoVZEvjejdNccFF8bdiCOqTgGn5SWOO" target="_blank">
+                        <img src="../../../../assets/img/footer/ytIncon.png" alt="Logo YouTube">
+                    </a>
+                    
+                    <a href="https://www.geogebra.org/m/tuyvrnbp" target="_blank">
+                        <img src="../../../../assets/img/footer/geogebraIncon2.png" alt="Logo Geogebra">
+                    </a>
+                </div>
+                <div><a href="https://www.uff.br/" target="_blank"><img class="logo-uff" src="../../../../assets/img/footer/uffIncon.png" alt="Logo da UFF"></a></div>
+            </div>
+        </footer>
+`;
+
 
 try {
     document.querySelector('.footer').innerHTML = conteudoFooter;
@@ -99,4 +128,8 @@ try {
 
 try {
     document.querySelector('.footer-nv3').innerHTML = conteudoFooterNv3;
+} catch{};
+
+try {
+    document.querySelector('.footer-nv4').innerHTML = conteudoFooterNv4;
 } catch{};
